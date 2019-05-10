a10764e1b1def5fce815c0d3ff48053ec2095e18
Adam Menges <adam@adammenges.com>
2019-05-10 13:49:29 -0700

    mobile!

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import useResizeAware from "react-resize-aware";
 
 export default () => {
   const [resizeListener, sizes] = useResizeAware();
-
-  return (
+  const mobile = sizes.width < 640;
+  const desktop = (
     <div
       style={{
         backgroundColor: "#00080e",
@@ -58,4 +58,59 @@ export default () => {
       </div>
     </div>
   );
+  const iphone = (
+    <div
+      style={{
+        backgroundColor: "#00080e",
+        height: "100vh", // maybe do this with code?
+        // height: sizes.height,
+        // width: sizes.width,
+        position: "relative",
+        // left: 0,
+        // top: 0
+        overflow: "hidden"
+      }}
+    >
+      {resizeListener}
+      <div
+        className="me-blend"
+        style={{
+          position: "absolute",
+          left: sizes.width / 2 - 225,
+          top: sizes.height / 2 - 225 - 75
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          left: sizes.width / 2 - 110,
+          top: sizes.height / 2 + 125
+        }}
+      >
+        <div
+          style={{
+            fontSize: 37
+          }}
+        >
+          Adam Menges
+        </div>
+        <div
+          style={{
+            fontSize: 17,
+            marginTop: -2
+          }}
+        >
+          adam@adammenges.com
+        </div>
+        <div
+          style={{
+            fontSize: 17
+          }}
+        >
+          720-484-0275
+        </div>
+      </div>
+    </div>
+  );
+  return mobile ? iphone : desktop;
 };
