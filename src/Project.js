3616cdd88dea6076e445a90cdb01e34204b6057e
Adam Menges <adam@adammenges.com>
2019-05-23 20:51:48 -0700

    GestureView

--- src/Project.js
+++ src/Project.js
@@ -1,22 +1,98 @@
 import React from 'react'
 import { useWindowSize } from './utils.js'
+import GestureView from 'react-gesture-view'
+
+export function TabContentThree() {
+  const [index, setIndex] = React.useState(0)
+  return (
+    <GestureView
+      value={index}
+      enableMouse={true}
+      onRequestChange={i => setIndex(i)}
+    >
+      <div>First panel</div>
+      <div>Second panel</div>
+      <div>Third panel</div>
+      {(props, active, load) => <div {...props}>fourth panel</div>}
+    </GestureView>
+  )
+}
+
+export function TabContent() {
+  const [index, setIndex] = React.useState(0)
+  const ref = React.useRef()
+
+  function focusCurrentIndex() {
+    ref.current.focus()
+  }
+
+  return (
+    <GestureView ref={ref} value={index} onRequestChange={i => setIndex(i)}>
+      <div>First panel</div>
+      <div>Second panel</div>
+      <div>Third panel</div>
+    </GestureView>
+  )
+}
+
+// aspect ratio of my MacBook, idk seems like a good place to start
+export const projectsWidth = 2304 / 4
+export const projectsHeight = 1440 / 4
 
 export const Project = () => {
+  const [index, setIndex] = React.useState(0)
   const size = useWindowSize()
-  const h = size.height
-  const w = size.width
-  const small = w <= 640
+  const small = size.width <= 640
+
+  const project1 = (
+    <div
+      style={{
+        width: projectsWidth,
+        height: projectsHeight,
+        backgroundColor: '#111',
+        color: '#FFF',
+        outline: 'none',
+      }}
+      className="noselect"
+    >
+      <div className="noselect">something111</div>
+    </div>
+  )
+  const project2 = (
+    <div
+      style={{
+        width: projectsWidth,
+        height: projectsHeight,
+        backgroundColor: '#777',
+        color: '#FFF',
+        outline: 'none',
+      }}
+      className="noselect"
+    >
+      <div className="noselect">something222</div>
+    </div>
+  )
 
   const desktop = (
     <div
       style={{
-        width: 100,
-        height: 100,
-        backgroundColor: '#345',
+        width: projectsWidth,
+        height: projectsHeight,
+        backgroundColor: '#555',
         color: '#FFF',
+        outline: 'none',
       }}
+      className="noselect"
     >
-      <div>something</div>
+      <GestureView
+        value={index}
+        enableMouse={true}
+        onRequestChange={i => setIndex(i)}
+      >
+        {project1}
+        {project2}
+        {/* {(props, active, load) => <div {...props}>fourth panel</div>} */}
+      </GestureView>
     </div>
   )
   const mobile = (
@@ -24,5 +100,9 @@ export const Project = () => {
       <div>something</div>
     </div>
   )
-  return <div style={{ position: 'relative' }}>{small ? desktop : desktop}</div>
+  return (
+    <div style={{ position: 'relative' }} className="noselect">
+      {small ? desktop : desktop}
+    </div>
+  )
 }
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react'
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from "@storybook/react/demo";
-import { Header } from "../Header";
-import { Project } from "../Project";
+import { Button, Welcome } from '@storybook/react/demo'
+import { Header } from '../Header'
+import { Project, projectsWidth, TabContent, TabContentThree } from '../Project'
 
-storiesOf("Welcome", module).add("to Storybook", () => (
-  <Welcome showApp={linkTo("Button")} />
-));
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
 
 // storiesOf("Button", module)
 //   .add("with text", () => (
@@ -24,6 +24,75 @@ storiesOf("Welcome", module).add("to Storybook", () => (
 //     </Button>
 //   ));
 
-storiesOf("Header", module).add("Usual Header", () => <Header />);
+storiesOf('Header', module).add('Usual Header', () => <Header />)
 
-storiesOf("Project", module).add("Usual Project", () => <Project />);
+storiesOf('Project', module)
+  .add('Usual Project', () => (
+    <div
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        paddingTop: '12px',
+        // margin: '0 auto',
+      }}
+      className="noselect"
+    >
+      <div
+        style={{
+          width: projectsWidth,
+          margin: '0 auto',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          // margin: '0 auto',
+        }}
+        className="noselect"
+      >
+        <Project />
+      </div>
+    </div>
+  ))
+  .add('Tab Project', () => (
+    <div
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        paddingTop: '12px',
+        // margin: '0 auto',
+      }}
+    >
+      <div
+        style={{
+          width: projectsWidth,
+          margin: '0 auto',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          // margin: '0 auto',
+        }}
+      >
+        <TabContent />
+      </div>
+    </div>
+  ))
+  .add('Tab Project Three', () => (
+    <div
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        paddingTop: '12px',
+        // margin: '0 auto',
+      }}
+    >
+      <div
+        style={{
+          width: projectsWidth,
+          margin: '0 auto',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          // margin: '0 auto',
+        }}
+      >
+        <TabContentThree />
+      </div>
+    </div>
+  ))
+  .add('Tab Project Four', () => <TabContentThree />)
