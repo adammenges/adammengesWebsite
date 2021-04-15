ee4815bc841d15cd84159b2a4878e82fbc51c3c6
Adam Menges <adam@adammenges.com>
2021-04-15 17:51:43 +0000

    idk stuff

---
+++ src/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactTooltip from 'react-tooltip'
+import { useWindowSize, sendEmail } from './utils.js'
+import { useState, useEffect } from 'react'
+
+export default FourOhFour = () => {
+  return <div style={{ position: 'relative' }}>shrug</div>
+}
---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactTooltip from 'react-tooltip'
+import { useWindowSize, sendEmail } from './utils.js'
+import { useState, useEffect } from 'react'
+
+// export const Portfolio = () => {
+//   return <div style={{ position: 'relative' }}>Hellooooooo</div>
+// }
+
+export default Portfolio = () => {
+  return <div style={{ position: 'relative' }}>Hellooooooo</div>
+}
